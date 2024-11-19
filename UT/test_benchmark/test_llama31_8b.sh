ad58d5f16186d5517f678c43d9df372807aafc1b
Pluto1944 <yangboqwer@gmail.com>
2024-11-19 14:53:49 +0800

    feat: benchmark - llama3 & llama3.1 7b H20 UT

--- UT/test_benchmark/test_llama31_8b.sh
+++ UT/test_benchmark/test_llama31_8b.sh
@@ -32,24 +32,7 @@ if [ $RUN_TILEARN -eq 1 ]; then
         export TILEARN_HYBRID_ZERO_STAGE=${TILEARN_HYBRID_ZERO_STAGE:-1}
 
     elif [ $GPU_MEM = "80G" ]; then
-
-        # llama factory model random initialization
-        export LF_MODEL_RANDOM_INIT=${LF_MODEL_RANDOM_INIT:-0}
-        export LF_UT_TEST=${LF_UT_TEST:-1}
-        export LF_UT_MODEL_PATH_PREFIX=${LF_UT_MODEL_PATH_PREFIX:-"../../models ||| /mnt/cfs/tilearn/pretrain_models"}
-        export LF_UT_DSYAML_PATH_PREFIX=${LF_UT_DSYAML_PATH_PREFIX:-"ds_z3_config.json ||| ds_z3_config.json"}
-        export LF_UT_DIS_GRAD_CKPT=${LF_UT_DIS_GRAD_CKPT:-1}
-        export LF_UT_MAX_LENGTH=${LF_UT_MAX_LENGTH:-4096}
-        export LF_UT_BS=${LF_UT_BS:-1}
-        export LF_UT_GRAD_ACC=${LF_UT_GRAD_ACC:-8}
-        export LF_UT_MAX_STEPS=${LF_UT_MAX_STEPS:-50}
-        export LF_UT_LOG_STEPS=${LF_UT_LOG_STEPS:-10}
-
-        export TILEARN_DEBUG=1
-        export TILEARN_HYBRID_TP_SIZE=${TILEARN_HYBRID_TP_SIZE:-1}
-        export TILEARN_HYBRID_PP_SIZE=${TILEARN_HYBRID_PP_SIZE:-2}
-        export TILEARN_HYBRID_OFFLOAD=${TILEARN_HYBRID_OFFLOAD:-0}
-        export TILEARN_HYBRID_ZERO_STAGE=${TILEARN_HYBRID_ZERO_STAGE:-1}
+        sleep 1s
 
     elif [ $GPU_MEM = "96G" ]; then
 
@@ -57,17 +40,18 @@ if [ $RUN_TILEARN -eq 1 ]; then
         export LF_MODEL_RANDOM_INIT=${LF_MODEL_RANDOM_INIT:-0}
         export LF_UT_TEST=${LF_UT_TEST:-1}
         export LF_UT_MODEL_PATH_PREFIX=${LF_UT_MODEL_PATH_PREFIX:-"../../models ||| /mnt/cfs/tilearn/pretrain_models"}
-        export LF_UT_DSYAML_PATH_PREFIX=${LF_UT_DSYAML_PATH_PREFIX:-"ds_z3_config.json ||| ds_z3_config.json"}
+        export LF_UT_DSYAML_PATH_PREFIX=${LF_UT_DSYAML_PATH_PREFIX:-"ds_z1_config.json ||| ds_z1_config.json"}
         export LF_UT_DIS_GRAD_CKPT=${LF_UT_DIS_GRAD_CKPT:-1}
         export LF_UT_MAX_LENGTH=${LF_UT_MAX_LENGTH:-4096}
         export LF_UT_BS=${LF_UT_BS:-1}
-        export LF_UT_GRAD_ACC=${LF_UT_GRAD_ACC:-8}
+        export LF_UT_GRAD_ACC=${LF_UT_GRAD_ACC:-4}
         export LF_UT_MAX_STEPS=${LF_UT_MAX_STEPS:-50}
         export LF_UT_LOG_STEPS=${LF_UT_LOG_STEPS:-10}
 
         export TILEARN_DEBUG=1
+        export TILEARN_HYBRID_MODE='None'
         export TILEARN_HYBRID_TP_SIZE=${TILEARN_HYBRID_TP_SIZE:-1}
-        export TILEARN_HYBRID_PP_SIZE=${TILEARN_HYBRID_PP_SIZE:-2}
+        export TILEARN_HYBRID_PP_SIZE=${TILEARN_HYBRID_PP_SIZE:-1}
         export TILEARN_HYBRID_OFFLOAD=${TILEARN_HYBRID_OFFLOAD:-0}
         export TILEARN_HYBRID_ZERO_STAGE=${TILEARN_HYBRID_ZERO_STAGE:-1}
 
@@ -104,18 +88,7 @@ elif [ $RUN_TILEARN -eq 0 ]; then
         export LF_UT_LOG_STEPS=${LF_UT_LOG_STEPS:-10}
 
     elif [ "$GPU_MEM" = "80G" ]; then
-
-        # llama factory model random initialization
-        export LF_MODEL_RANDOM_INIT=${LF_MODEL_RANDOM_INIT:-0}
-        export LF_UT_TEST=${LF_UT_TEST:-1}
-        export LF_UT_MODEL_PATH_PREFIX=${LF_UT_MODEL_PATH_PREFIX:-"../../models ||| /mnt/cfs/tilearn/pretrain_models"}
-        export LF_UT_DSYAML_PATH_PREFIX=${LF_UT_DSYAML_PATH_PREFIX:-"ds_z3_config.json ||| ds_z3_config.json"}
-        export LF_UT_DIS_GRAD_CKPT=${LF_UT_DIS_GRAD_CKPT:-1}
-        export LF_UT_MAX_LENGTH=${LF_UT_MAX_LENGTH:-4096}
-        export LF_UT_BS=${LF_UT_BS:-1}
-        export LF_UT_GRAD_ACC=${LF_UT_GRAD_ACC:-4}
-        export LF_UT_MAX_STEPS=${LF_UT_MAX_STEPS:-50}
-        export LF_UT_LOG_STEPS=${LF_UT_LOG_STEPS:-10}
+        sleep 1s
 
     elif [ "$GPU_MEM" = "96G" ]; then
 
@@ -123,7 +96,7 @@ elif [ $RUN_TILEARN -eq 0 ]; then
         export LF_MODEL_RANDOM_INIT=${LF_MODEL_RANDOM_INIT:-0}
         export LF_UT_TEST=${LF_UT_TEST:-1}
         export LF_UT_MODEL_PATH_PREFIX=${LF_UT_MODEL_PATH_PREFIX:-"../../models ||| /mnt/cfs/tilearn/pretrain_models"}
-        export LF_UT_DSYAML_PATH_PREFIX=${LF_UT_DSYAML_PATH_PREFIX:-"ds_z3_config.json ||| ds_z3_config.json"}
+        export LF_UT_DSYAML_PATH_PREFIX=${LF_UT_DSYAML_PATH_PREFIX:-"ds_z1_config.json ||| ds_z1_config.json"}
         export LF_UT_DIS_GRAD_CKPT=${LF_UT_DIS_GRAD_CKPT:-1}
         export LF_UT_MAX_LENGTH=${LF_UT_MAX_LENGTH:-4096}
         export LF_UT_BS=${LF_UT_BS:-1}
