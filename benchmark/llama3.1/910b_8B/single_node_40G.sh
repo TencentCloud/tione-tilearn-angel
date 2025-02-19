28206fa3e7eb2bfb1d2ecd23a4d77527d9d63236
Pluto1944 <yangboqwer@gmail.com>
2025-02-19 11:54:42 +0800

    feat: UT benchmark - add llama3.1/910b_8B && 3D parallel

---
+++ benchmark/llama3.1/910b_8B/single_node_40G.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# tione.tencentcloudcr.com/qcloud-ti-platform/llm-train:24.03-gpu-py310-cu124-tilearn-llm-v1.8.0
+# cd LLaMA-Factory
+# pip3 install -e ".[torch,metrics]"
+# transformers 4.39.3
+
+### Demo Args
+# llama factory model random initialization
+export LF_MODEL_RANDOM_INIT=1
+#export LF_UT_TEST=1
+#export LF_UT_MODEL_PATH_PREFIX="../../models ||| /mnt/cfs/tilearn/pretrain_models"
+
+CODE_PATH=../../utils/train_baseline.py
+YAML_PATH=full_sft_baseline_40G.yaml
+LOG_PATH=./log/baseline_40G.log
+
+export GPUS_NUM_PER_NODE=8
+
+bash ../../utils/run_single_node.sh $CODE_PATH $YAML_PATH $LOG_PATH
+exit $?
